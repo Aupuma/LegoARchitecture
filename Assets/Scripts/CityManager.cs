63bdec1eaa99c638681e89be2b8f909f6eca8041
Aupuma <aurepuerta@gmail.com>
2019-12-10 01:04:08 +0100

    Imported San Francisco set and added scripts for managing animation and asset references

--- Assets/Scripts/CityManager.cs
+++ Assets/Scripts/CityManager.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class CityManager : MonoBehaviour
 {
     public CityData[] dataContainers;
+    public GameObject[] buildingWorldDataObjs;
+    private int currentDataIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (dataContainers.Length != buildingWorldDataObjs.Length)
+        {
+            Debug.LogError("TAMAÑO DE ARRAYS DE DATOS DE EDIFICIOS NO COINCIDE");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowNextData()
     {
-        
+        currentDataIndex++;
+    }
+
+    public void ShowPreviousData()
+    {
+
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    #region SINGLETON
+    public static UIManager instance;
+
+    private void Awake()
     {
-        
+        instance = this;
+    }
+    #endregion
+
+    public Transform cityInfoTransform;
+    public TextMeshProUGUI buildingNameText;
+    public TextMeshProUGUI buildingDescriptionText;
+    public Button leftButton;
+    public Button RightButton;
+    private Animator animator;
+
+    private CityManager currentCityManager;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowPreviousBuilding()
     {
-        
+
+    }
+
+    public void ShowNextBuilding()
+    {
+
+    }
+
+    public void SetScreenInfoVisibility(bool newVisibility)
+    {
+        animator.SetBool("InfoActive", newVisibility);
+    }
+
+    public void SetActionBarVisibility(bool newVisibility)
+    {
+        animator.SetBool("ActionBarActive", newVisibility);
+    }
+
+    public void SetLeftButtonVisibility(bool newVisibility)
+    {
+        leftButton.gameObject.SetActive(newVisibility);
+    }
+
+    public void SetRightButtonVisibility(bool newVisibility)
+    {
+        leftButton.gameObject.SetActive(newVisibility);
+    }
+
+    public void SetCityManager(CityManager cityManager)
+    {
+        if(cityManager != currentCityManager)
+        {
+
+        }
+        currentCityManager = cityManager;
+
     }
 }
